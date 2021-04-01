5161c08e05557f23c50d22fa822467374dfd6483
ymym <ymym@kakao.com>
2021-04-01 16:53:02 +0900

    일정 관련 front

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Todo list</h1>
+      <input type="text" class="form-control mb-5" v-model="userInput" @keydown.enter="addTodo">
+
+      <div class="list-group mb-5">
+        <template v-for="todo in nonCompletedTodo">
+          <Todo
+              :value="todo.value" v-bind:key="todo.id"
+              @component-click="clickTodo(todo)"
+          />
+        </template>
+      </div>
+
+      <div class="text-right">
+        <button type="button" class="btn" :class="{'btn-primary': type === false, 'btn-secondary': type !== false}" @click="changeType(false)">미완료</button>
+        <button type="button" class="btn" :class="{'btn-primary': type === true, 'btn-secondary': type !== true}" @click="changeType(true)">완료</button>
+        <button type="button" class="btn" :class="{'btn-primary': type === 'all', 'btn-secondary': type !== 'all'}" @click="changeType('all')">전체</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from './components/Todo';
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      userInput: '',
+      type: false,
+      todoList: []
+    };
+  },
+  computed: {
+    nonCompletedTodo() {
+      return this.todoList.filter(todo => this.type === 'all' || todo.done === this.type);
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todoList.push({
+        value: this.userInput,
+        done: false
+      });
+      this.userInput = '';
+    },
+    changeType(type) {
+      this.type = type;
+    },
+    clickTodo(todo) {
+      todo.done = !todo.done;
+    }
+  },
   components: {
-    HelloWorld
+    Todo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" class="list-group-item text-left" @click="click">{{ value }}</button>
+</template>
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    click() {
+      this.$emit('component-click');
+    }
+  }
+};
+</script>
